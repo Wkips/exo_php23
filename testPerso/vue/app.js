42f20c59c5483f23ebc48926d828a341c53de8c9
Wkips <willy_kips@hotmail.fr>
2023-03-09 09:59:42 +0100

    test

---
+++ testPerso/vue/app.js
@@ -0,0 +1,9 @@
+// Fonction qui permet d'initialiser Vue.js
+Vue.createApp({
+    data() {
+        return {
+            title: "Hello Word Vue"
+        }
+    }
+}).mount('#app') 
+// mount spécifie que l'application Vue s'affichera dans la div avec id="app"
